bd18d2c10d1c3a4bb4fe8b3da6e0a378637e18a3
eyjhbb@gmail.com <eyjhbb@gmail.com>
2019-03-29 13:21:00 +0100

    added netcat

---
+++ netcat/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+DOCKER_USER="eyjhb"
+DOCKER_NAME="netcat"
+
+# get versions
+VERSION="$(awk '$2 == "VERSION" { print $3; exit }' Dockerfile)"
+
+# build the image
+docker build -t $DOCKER_USER/$DOCKER_NAME:latest .
+docker build -t $DOCKER_USER/$DOCKER_NAME:$VERSION .
+
+# push the images
+docker push $DOCKER_USER/$DOCKER_NAME:latest
+docker push $DOCKER_USER/$DOCKER_NAME:$VERSION
---
+++ netcat/readme.md
@@ -0,0 +1,3 @@
+# Netcat
+A simple netcat server, that will run on port 8080!
+Useful for quickly testing if port binding works in Docker etc.

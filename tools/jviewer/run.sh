5554d28c0b0caad4b339395b775f6c55f2e4be26
eyjhb <eyjhbb@gmail.com>
2026-05-24 17:23:33 +0200

    updated jviewer

--- tools/jviewer/run.sh
+++ tools/jviewer/run.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# nix-shell -p xhost --run 'xhost +local:'
+# _JAVA_AWT_WM_NONREPARENTING=1 javaws jviewer.jnlp
 docker run --rm -it \
     --net=host \
     --env="DISPLAY=:0" \
